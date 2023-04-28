c99c02ef9960628d4563dcf2e70595d3c0b37a9d
ufuk-cakir <ufukcakir2001@gmail.com>
2023-04-28 15:06:35 +0200

    init

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,14 +13,14 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../src/mypackage"))
+sys.path.insert(0, os.path.abspath("../../src/megs"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = "python-project-template"
-copyright = "2020, Scientific Software Center"
-author = "Scientific Software Center"
+project = "MEGS: Morphological Evaluation of Galatic Structures"
+copyright = "2023, Interdisciplinary Center for Scientific Computing"
+author = "Ufuk Cakir"
 
 # The full version, including alpha/beta/rc tags
 release = "0.1"
